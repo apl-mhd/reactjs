a8501fc53d7780ea93d593f442f22548c420b7b1
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-04-26 15:01:10 +0600

    done with lifting state

--- src/Lifting.js
+++ src/Lifting.js
@@ -1,7 +1,54 @@
+import { useState } from "react"
 import React from "react"
 export default function Lifting(){
+    const [todos, setTodos] =  useState(["item 1", "item 2", "item 3"])
 
     return(
-        <h1>hello</h1>
+        <>
+            <TodoCount todos={todos}/>
+            <TodoList todos={todos} />
+            <AddTodo setTodos={setTodos}/>
+        </>
+    )
+}
+
+function del(e){
+
+    e.preventDefault()
+    console.log(e.target.value.key)
+}
+
+
+function TodoCount(props){
+    return <div>Total Todos: {props.todos.length} </div>
+}
+
+
+function TodoList(props){
+    return(
+        <ul>
+            {props.todos.map((todo, index) =>{
+                
+                return(
+                <li onClick={del} key={index}>{todo}</li>
+                )
+                })}
+        </ul>
+    )
+}
+
+function AddTodo(props){
+    function handleSubmit(e){
+        e.preventDefault();
+        const todo = e.target.elements.todo.value
+        console.log(todo)
+        props.setTodos(prevTodos => [...prevTodos, todo])
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type="text" id="todo"/>
+            <input type="submit" value="Submit"/>
+        </form>
     )
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Lifting from './Lifting';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render()
+root.render(<Lifting/>)
 
 
 

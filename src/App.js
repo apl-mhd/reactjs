6803e55cb183cdf7c9aaf8bed9ab646c408f1358
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-04-27 15:03:30 +0600

    image upload done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from 'axios';
+ 
 
 
 class App extends React.Component{
@@ -15,9 +17,12 @@ class App extends React.Component{
          image: null,
          users:[]
        } 
+
+
      
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.fileChange = this.fileChange.bind(this)
     this.getCookie = this.getCookie.bind(this)
     this.fetchUsers = this.fetchUsers.bind(this)
 
@@ -48,7 +53,6 @@ fetchUsers(){
   fetch('http://127.0.0.1:8000/api/auth/users/')
   .then(response => response.json())
   .then(data => {
-    //console.log(data)
     this.setState({
       users: data
     })
@@ -59,37 +63,62 @@ fetchUsers(){
   handleChange(e){
     var name = e.target.name
     var value = e.target.value
+    
     this.setState({
       [name]: value
     })
-    console.log(JSON.stringify(this.state))
+        console.log(this.state)
+
+  }
+
+
+  fileChange(e){
+   
+      this.setState({ image: e.target.files[0] });
   }
 
   handleSubmit(e){
+
+
     e.preventDefault();
-      var csrftoken = this.getCookie('csrftoken')
+    const formData = new FormData();
     var url = 'http://127.0.0.1:8000/api/auth/create-user/'
 
-    fetch(url, {
-      method: 'POST',
-      headers:{
-          'Content-type':'application/json',
-          'X-CSRFToken':csrftoken,
-      },
-      body:JSON.stringify(this.state)
-    }).then((res)=>{
-      this.fetchUsers()
-      this.setState({
-         password: '',
-         first_name: '',
-         last_name: '',
-         username: '',
-         email:'',
-         image: null,
-      })
-    }).catch(function(error){
-      console.log('error', error)
-    })
+
+    formData.append('password', this.state.password);
+    formData.append('username', this.state.username);
+    formData.append('first_name', this.state.first_name);
+    formData.append('last_name', this.state.last_name);
+    formData.append('email', this.state.email);
+		formData.append('image', this.state.image);
+		axios.post(url, formData);
+    this.fetchUsers()
+
+   
+      var csrftoken = this.getCookie('csrftoken')
+      axios.post(url, formData);
+
+
+    // fetch(url, {
+    //   method: 'POST',
+    //   headers:{
+    //       'Content-type':'application/json',
+    //       'X-CSRFToken':csrftoken,
+    //   },
+    //   body:JSON.stringify(this.state)
+    // }).then((res)=>{
+    //   this.fetchUsers()
+    //   this.setState({
+    //      password: '',
+    //      first_name: '',
+    //      last_name: '',
+    //      username: '',
+    //      email:'',
+    //      image: null,
+    //   })
+    // }).catch(function(error){
+    //   console.log('error', error)
+    // })
     
   }
 
@@ -142,10 +171,10 @@ fetchUsers(){
             Email:
             <input onChange={this.handleChange} type="email" name="email"/><br/>
           </label>
-          {/* <label>
+           <label>
             Photo:
-            <input onChange={this.handleChange} type="file" name="image"/><br/>
-          </label> */}
+            <input onChange={this.fileChange} type="file" name="image"/><br/>
+          </label> 
         
           <input type="submit" value="Submit"/>
         </form>
